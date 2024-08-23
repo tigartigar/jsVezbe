9f35e0fa2b5a7138e743eb490f3943efdd1c5801
tigartigar <tigar1389tigar@gmail.com>
2024-08-23 16:46:52 +0200

    adding fundamentals 1 folder

---
+++ starter/script.js
@@ -0,0 +1,68 @@
+// let js = 'amazing';
+// console.log('Tigar tigar');
+
+// let firstName = "Tigar";
+// console.log("De si", firstName, "bre 100 godina");
+
+// let country = 'Serbia';
+// let continent = 'Europe';
+// let population = 6.7;
+
+// console.log('I live in a country called', country, ', it is located in', continent, 'and has the population around', population, 'million people.')
+
+// let tigarJeNajjaci = true;
+// console.log("Tigar je najjaci: ",tigarJeNajjaci);
+
+// tigarJeNajjaci = "Da, naravno";
+// console.log(typeof tigarJeNajjaci);
+// let lav;
+// console.log(typeof lav);
+
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1993;
+// //birthYear = 1990
+
+// let now = 2037;
+// let ageMarko = now - 1993;
+// let ageDejan = now - 1969;
+
+// console.log("In year", now, "Marko will have", ageMarko, "years, and his father Dejan will have", ageDejan, "years");
+
+// console.log("9 na kvadrat je", 9 ** 2)
+
+// console.log(now - 1991 > now - 2018);
+
+// const firstName = "Marko";
+// const job = "teacher";
+// const birthYear = 1993;
+// const year = 2024
+
+// const intro = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "."
+
+// console.log(intro)
+
+// const newIntro = `I'm ${firstName}, a ${year-birthYear} years old ${job}.`;
+// console.log(newIntro);
+// console.log(`de si tigre legendo moja!`)
+
+let age = 14;
+
+if(age >= 18) {
+    console.log(`You can start driving! 🚗 - You are ${age} years old!`);
+} else {
+    console.log(`You're not old enough 🔞 - You only have ${age} years......you can take your licence in ${18-age} years..`);
+}
+
+
+const birthYear = 2012;
+
+if(birthYear <= 2000){
+    let century = 20
+    console.log(`You're born in ${century}'th century. `)
+} else{
+    let century = 21
+    console.log(`You're a millenial`)
+}
